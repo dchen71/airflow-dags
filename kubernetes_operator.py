02e9eefed53b2b6d58e24864c2d094e909a8968e
Daniel Chen <dchen12812@gmail.com>
2020-02-28 13:49:14 -0800

    more testing

--- kubernetes_operator.py
+++ kubernetes_operator.py
@@ -1,16 +1,45 @@
+from airflow import DAG
+from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow import configuration as conf
 
 
-namespace = conf.get('kubernetes', 'default')
-k = kubernetes_pod_operator.KubernetesPodOperator(
-    namespace=namespace,
-    image="ubuntu:16.04",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10", "echo pwd"],
-    labels={"foo": "bar"},
-    name="airflow-test-pod",
-    is_delete_operator_pod=True,
-    in_cluster=True,
-    task_id="task-two",
-    get_logs=True)
+# This will detect the default namespace locally and read the 
+# environment namespace when deployed to Astronomer.
+if namespace =='default':
+    config_file = '/usr/local/airflow/include/.kube/config'
+    in_cluster=False
+else:
+    in_cluster=True
+    config_file=None
+
+
+args = {
+    'owner': 'airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='example_kubernetes_pod',
+    default_args=args,
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+namespace = conf.get('kubernetes', 'NAMESPACE')
+#namespace = 'default'
+compute_resource = {'request_cpu': '800m', 'request_memory': '3Gi', 'limit_cpu': '800m', 'limit_memory': '3Gi'}
+
+with dag:
+    k = KubernetesPodOperator(
+        namespace=namespace,
+        image="hello-world",
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task-one",
+        in_cluster=in_cluster # if set to true, will look in the cluster, if false, looks for file
+        cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
+        config_file=config_file,
+        resources=compute_resource,
+        is_delete_operator_pod=True,
+        get_logs=True)
