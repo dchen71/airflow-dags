9caed4dd4a34c8f997f0dc7cb17d8e7ffd1b14d6
Daniel Chen <dchen12812@gmail.com>
2020-04-24 17:36:56 -0700

    change limits again

--- rnaseq1.py
+++ rnaseq1.py
@@ -95,7 +95,7 @@ with DAG(
         cmds=["bash /rnaseq/rnaseq2020.sh"],
         volumes=[input_sample_volume],
         volume_mounts=[input_sample_mount],
-        resources={'request_memory':'31Gi', 'request_cpu': '4000m'},
+        resources={'request_memory':'30Gi', 'request_cpu': '4', 'limit_cpu': '4'},
         is_delete_operator_pod=True
     )
 
