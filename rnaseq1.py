4bbfe324bd2414f5814338514ece84c79a95a7ab
Daniel Chen <dchen12812@gmail.com>
2020-04-24 19:27:34 -0700

    debug mode

--- rnaseq1.py
+++ rnaseq1.py
@@ -92,11 +92,11 @@ with DAG(
         name = "rnaseq1_pipeline",
         namespace='default',
         image="airflow1.azurecr.io/rnaseq:202003",
-        cmds=["bash /rnaseq/rnaseq2020.sh"],
+        cmds=["/bin/bash /rnaseq/rnaseq2020.sh"],
         volumes=[input_sample_volume, input_ref_volume, input_data_volume, output_volume],
         volume_mounts=[input_sample_mount, input_ref_mount, input_data_mount, output_mount],
         resources={'request_memory':'24Gi', 'limit_memory': '30G', 'request_cpu': '4', 'limit_cpu': '4'},
-        is_delete_operator_pod=True
+        is_delete_operator_pod=False
     )
 
     # Order for pipeline to do stuff
