4c5a35cb81b5e7570682ce0097e8f4f13c8e775f
Daniel Chen <dchen12812@gmail.com>
2020-04-24 17:24:35 -0700

    comment out others

--- rnaseq1.py
+++ rnaseq1.py
@@ -14,63 +14,63 @@ from airflow.contrib.kubernetes.volume_mount import VolumeMount
 # Persistent Volume Configuration
 ##
 
-## Input sample table
-#input_sample_config= {
-#    'persistentVolumeClaim':
-#      {
-#        'claimName': 'pvc-input'
-#      }
-#    }
-#
-#input_sample_volume = Volume(name='input-sample-mount', configs=input_sample_config)
-#input_sample_mount = VolumeMount(name='input-sample-mount',
-#                                mount_path='/rnaseq',
-#                                sub_path=None,
-#                                read_only=True)
-
-## Reference Volume
-input_ref_config= {
-    'persistentVolumeClaim':
-      {
-        'claimName': 'pvc-references'
-      }
-    }
-
-input_ref_volume = Volume(name='reference-mount', configs=input_ref_config)
-input_ref_mount = VolumeMount(name='reference-mount',
-                              mount_path='/rnaseq/ref',
-                              sub_path=None,
-                              read_only=True)
-
-# Input Data Volume
-input_data_config= {
+# Input sample table
+input_sample_config= {
     'persistentVolumeClaim':
       {
         'claimName': 'pvc-input'
       }
     }
 
-input_data_volume = Volume(name='input-mount', configs=input_data_config)
-input_data_mount = VolumeMount(name='input-mount',
-                                mount_path='/rnaseq/data',
+input_sample_volume = Volume(name='input-sample-mount', configs=input_sample_config)
+input_sample_mount = VolumeMount(name='input-sample-mount',
+                                mount_path='/rnaseq',
                                 sub_path=None,
                                 read_only=True)
-
-## Output Volume
-output_config= {
-    'persistentVolumeClaim':
-      {
-        'claimName': 'pvc-output'
-      }
-    }
-
-output_volume = Volume(name='output-mount', configs=output_config)
-output_mount = VolumeMount(name='output-mount',
-                            mount_path='/rnaseq/output',
-                            sub_path=None,
-                            read_only=False)
-
-
+#
+### Reference Volume
+#input_ref_config= {
+#    'persistentVolumeClaim':
+#      {
+#        'claimName': 'pvc-references'
+#      }
+#    }
+#
+#input_ref_volume = Volume(name='reference-mount', configs=input_ref_config)
+#input_ref_mount = VolumeMount(name='reference-mount',
+#                              mount_path='/rnaseq/ref',
+#                              sub_path=None,
+#                              read_only=True)
+#
+## Input Data Volume
+#input_data_config= {
+#    'persistentVolumeClaim':
+#      {
+#        'claimName': 'pvc-input'
+#      }
+#    }
+#
+#input_data_volume = Volume(name='input-mount', configs=input_data_config)
+#input_data_mount = VolumeMount(name='input-mount',
+#                                mount_path='/rnaseq/data',
+#                                sub_path=None,
+#                                read_only=True)
+#
+### Output Volume
+#output_config= {
+#    'persistentVolumeClaim':
+#      {
+#        'claimName': 'pvc-output'
+#      }
+#    }
+#
+#output_volume = Volume(name='output-mount', configs=output_config)
+#output_mount = VolumeMount(name='output-mount',
+#                            mount_path='/rnaseq/output',
+#                            sub_path=None,
+#                            read_only=False)
+#
+#
 args = {
     'owner': 'airflow',
     'start_date': days_ago(2)
@@ -93,8 +93,8 @@ with DAG(
         namespace='default',
         image="airflow1.azurecr.io/rnaseq:202003",
         cmds=["bash /rnaseq/rnaseq2020.sh"],
-        volumes=[input_ref_config, input_data_volume, output_volume],
-        volume_mounts=[input_ref_mount, input_data_mount, output_mount],
+        volumes=[input_ref_config],
+        volume_mounts=[input_ref_mount],
         resources={'request_memory':'32Gi', 'request_cpu': '4000m'},
         is_delete_operator_pod=True
     )
