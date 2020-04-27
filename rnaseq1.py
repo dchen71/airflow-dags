237862ad537d55f7bb694252180d116597b52554
Daniel Chen <dchen12812@gmail.com>
2020-04-27 15:09:22 -0700

    remove sample thing

--- rnaseq1.py
+++ rnaseq1.py
@@ -147,8 +147,8 @@ with DAG(
         image="ubuntu:18.04",
         cmds=["df"],
         arguments=["-h"],
-        volumes=[input_sample_volume, input_ref_volume, input_data_volume, output_volume],
-        volume_mounts=[input_sample_mount, input_ref_mount, input_data_mount, output_mount],
+        volumes=[input_ref_volume, input_data_volume, output_volume],
+        volume_mounts=[input_ref_mount, input_data_mount, output_mount],
         resources={'request_memory':'1Gi', 'limit_memory': '1Gi', 'request_cpu': '1', 'limit_cpu': '1'},
         is_delete_operator_pod=False
     )
@@ -159,8 +159,8 @@ with DAG(
         namespace='default',
         image="dchen71/rna:202003",
         #cmds=["/bin/bash /rnaseq/scripts/rnaseq2020.sh"],
-        volumes=[input_sample_volume, input_ref_volume, input_data_volume, output_volume],
-        volume_mounts=[input_sample_mount, input_ref_mount, input_data_mount, output_mount],
+        volumes=[input_ref_volume, input_data_volume, output_volume],
+        volume_mounts=[input_ref_mount, input_data_mount, output_mount],
         resources={'request_memory':'24Gi', 'limit_memory': '30G', 'request_cpu': '4', 'limit_cpu': '4'},
         is_delete_operator_pod=False
     )
