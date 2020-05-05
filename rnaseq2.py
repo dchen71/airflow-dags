08f44e02a5fb4df8d27cb36effd90f21d155eac4
Daniel Chen <dchen12812@gmail.com>
2020-05-05 12:15:42 -0700

    debug

--- rnaseq2.py
+++ rnaseq2.py
@@ -129,7 +129,7 @@ with DAG(
         volumes=[input_ref_config, input_data_volume, output_volume],
         volume_mounts=[input_ref_mount, input_data_mount, output_mount],
         resources = {'limit_cpu': '8000m', 'limit_memory': '32Gi'},
-        is_delete_operator_pod=True
+        is_delete_operator_pod=False
     )
 
     # SALMON
@@ -195,7 +195,7 @@ with DAG(
         "-o /mnt/output/{{ti.xcom_pull(task_ids = 'parse_filename')}}/fastqc"],
         volumes=[input_data_volume, output_volume],
         volume_mounts=[input_data_mount, output_mount],
-        is_delete_operator_pod=True
+        is_delete_operator_pod=False
     )
 
     # Samtools sort
