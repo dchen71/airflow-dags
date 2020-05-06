e730191e7543674a21a701420e1614c7d32760ce
Daniel Chen <dchen12812@gmail.com>
2020-05-06 15:11:23 -0700

    fix name

--- rnaseq2.py
+++ rnaseq2.py
@@ -122,7 +122,7 @@ with DAG(
         "--runThreadN", "7",
         "--readFilesCommand", "zcat", 
         "--readFilesIn", "/mnt/data/{{ dag_run.conf['read1_name'] }}", "/mnt/data/{{ dag_run.conf['read2_name'] }}", 
-        "--outFileNamePrefix", "/mnt/output/{{ti.xcom_pull(task_ids = 'parse_filename')}}/star",
+        "--outFileNamePrefix", "/mnt/output/{{ti.xcom_pull(task_ids = 'parse_filename')}}/star/",
         "--outSAMunmapped", "Within",
         "--outSAMtype", "BAM", "SortedByCoordinate",
         "--quantMode", "TranscriptomeSAM", "GeneCounts"],
