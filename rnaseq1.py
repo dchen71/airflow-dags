7ad520e5f9f8bf46e9f8e214085b882705d2d392
Biao Li <bli03@gilead.com>
2020-09-11 09:39:32 -0700

    Replace output subfolder with username

--- rnaseq1.py
+++ rnaseq1.py
@@ -1,5 +1,5 @@
 """
-RNASeq 1
+FASTQC for RNA-seq
 """
 import os
 
@@ -8,83 +8,103 @@ from airflow.utils.dates import days_ago
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.contrib.kubernetes.volume import Volume
 from airflow.contrib.kubernetes.volume_mount import VolumeMount
-
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 ##
 # Persistent Volume Configuration
-##
-
-## Reference Volume
+# Input and reference Volume
 input_ref_config= {
     'persistentVolumeClaim':
-      {
-        'claimName': 'pvc-references'
-      }
+    {
+        'claimName': 'blob-storage-pvc-input'
     }
+}
 
-input_ref_volume = Volume(name='reference-mount', configs=input_ref_config)
-input_ref_mount = VolumeMount(name='reference-mount',
-                              mount_path='/rnaseq/ref',
-                              sub_path='ref',
-                              read_only=True)
+input_ref_mount = VolumeMount(
+    name='input-ref-mount',
+    mount_path='/mnt/input',
+    sub_path=None,
+    read_only=True
+)
+input_ref_volume = Volume(name='input-ref-mount', configs=input_ref_config)
 
-# Input Data Volume
-input_data_config= {
-    'persistentVolumeClaim':
-      {
-        'claimName': 'pvc-input'
-      }
-    }
-
-input_data_volume = Volume(name='input-mount', configs=input_data_config)
-input_data_mount = VolumeMount(name='input-mount',
-                                mount_path='/rnaseq/data',
-                                sub_path=None,
-                                read_only=True)
-
-### Output Volume
+# Output Volume
 output_config= {
     'persistentVolumeClaim':
-      {
-        'claimName': 'pvc-output'
-      }
+    {
+        'claimName': 'blob-storage-pvc-output'
     }
+}
 
+output_mount = VolumeMount(
+    name='output-mount',
+    mount_path='/mnt/output',
+    sub_path=None,
+    read_only=False)
 output_volume = Volume(name='output-mount', configs=output_config)
-output_mount = VolumeMount(name='output-mount',
-                            mount_path='/rnaseq/output',
-                            sub_path=None,
-                            read_only=False)
-
 
 args = {
     'owner': 'airflow',
     'start_date': days_ago(2)
 }
 
-
 ##
-# RNA Seq 1
+# RNA-seq using Fastqc
 ##
 with DAG(
-    dag_id='rnaseq1',
+    dag_id='rnaseq_fastqc',
     default_args=args,
     schedule_interval=None,
-    tags=['example'],
+    tags=['fastqc'],
 ) as dag:
 
-    rna_seq = KubernetesPodOperator(
-        task_id="rna_seq_fat",
-        name = "rnaseq1_pipeline",
+    # Parse main file name without extensions
+    # As xcom_push is True, the last line written to stdout, the filename
+    # without suffix, will be pushed to an XCom
+    parse_filename = BashOperator(
+        task_id = 'parse_filename',
+        bash_command = "filename={{ dag_run.conf['read1_name'] }}; echo ${filename%%_R1*}",
+        xcom_push = True
+    )
+
+    # FASTQC
+    ## Create fastqc empty directory
+    create_fastqc_dir = KubernetesPodOperator(
+        task_id="create_fastqc_dir",
+        name = "rnaseq_create_fastqc_dir",
         namespace='default',
-        image="dchen71/rna:202003",
-        volumes=[input_ref_volume, input_data_volume, output_volume],
-        volume_mounts=[input_ref_mount, input_data_mount, output_mount],
-        resources={'request_memory':'24Gi', 'limit_memory': '32Gi', 'request_cpu': '4', 'limit_cpu': '4'},
+        image="ubuntu:18.04",
+        cmds=["mkdir"],
+        arguments=["-p", "/mnt/output/yajia/{{ti.xcom_pull(task_ids='parse_filename')}}/fastqc"],
+        volumes=[output_volume],
+        volume_mounts=[output_mount],
+        resources = {'request_cpu': '50m', 'request_memory': '50Mi'},
         is_delete_operator_pod=True
     )
 
-    # Order for pipeline to do stuff
-    ## ls mount > create files > write to files
-    rna_seq
-    
+    ## FASTQC
+    run_fastqc = KubernetesPodOperator(
+        task_id="run_fastqc",
+        name = "rnaseq_fastqc",
+        namespace='default',
+        image="quay.io/biocontainers/fastqc:0.11.9--0",
+        cmds=["fastqc"],
+        arguments=[
+            "/mnt/input/rnaseq_data/{{ dag_run.conf['read1_name'] }}",
+            "/mnt/input/rnaseq_data/{{ dag_run.conf['read2_name'] }}",
+            "-o", "/mnt/output/yajia/{{ti.xcom_pull(task_ids='parse_filename')}}/fastqc",
+            "-t", "2"
+        ],
+        volumes=[input_ref_volume, output_volume],
+        volume_mounts=[input_ref_mount, output_mount],
+        resources = {'request_cpu': '2'},
+        is_delete_operator_pod=True
+    )
+
+    ## Dummies
+    be_done = DummyOperator(
+        task_id = "done"
+    )
+
+    parse_filename >> create_fastqc_dir >> run_fastqc >> be_done
